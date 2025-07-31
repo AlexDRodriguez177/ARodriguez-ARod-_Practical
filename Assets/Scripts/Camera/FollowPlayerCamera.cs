33d2964b25724c7a45c84492baeae089c04d05e6
Alex Rodriguez <alex.rodriguez6@g.austincc.edu>
2025-07-31 18:45:40 -0500

    Added summary notes to my follow camera

--- Assets/Scripts/Camera/FollowPlayerCamera.cs
+++ Assets/Scripts/Camera/FollowPlayerCamera.cs
@@ -5,11 +5,19 @@ public class FollowPlayerCamera : MonoBehaviour
     public Transform player;
     public float smoothing = 5f;
     Vector3 offset;
+    /// <summary>
+    /// Sets the offset between the camera and the player 
+    /// Camera will stay at the position while following the player
+    /// </summary>
     void Start()
     {
         offset = transform.position - player.position;
     }
-
+    /// <summary>
+    /// Creates a smooth transition when following the player 
+    /// Creates a new position bassed off the player and offset
+    /// Usng Lerp to create a smooth transition
+    /// </summary>
     void Update()
     {
         Vector3 playerCameraPosition = player.position + offset;
